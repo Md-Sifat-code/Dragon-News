3fbd2fa146713bbcc2cf40d72a4fd03a28029e25
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-14 15:01:59 +0600

    adding the moment js

--- src/Componenets/Fixed_componenet/Navbar.jsx
+++ src/Componenets/Fixed_componenet/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "/assets/logo.png";
+import moment from "moment";
 export default function Navbar() {
   return (
     <section>
@@ -12,6 +13,7 @@ export default function Navbar() {
         <h1 className="text-center font-normal text-gray-500">
           Journalism Without Fear or Favour
         </h1>
+        <p>{moment().format("dddd, MMMM Do YYYY")}</p>
       </div>
     </section>
   );
