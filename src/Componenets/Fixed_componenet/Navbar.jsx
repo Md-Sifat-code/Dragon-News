6589b4c975df09ffe65f314421c396d3dd0997c1
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-14 14:55:42 +0600

    added the logo in the navbar

--- src/Componenets/Fixed_componenet/Navbar.jsx
+++ src/Componenets/Fixed_componenet/Navbar.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-
+import logo from "/assets/logo.png";
 export default function Navbar() {
-  return <div>This is Navbar</div>;
+  return (
+    <section>
+      <div className="container mx-auto flex flex-col gap-3 justify-center items-center py-6">
+        <img
+          className="md:w-96"
+          src={logo}
+          alt="This is the logo of this websiite"
+        />
+        <h1 className="text-center font-normal text-gray-500">
+          Journalism Without Fear or Favour
+        </h1>
+      </div>
+    </section>
+  );
 }
