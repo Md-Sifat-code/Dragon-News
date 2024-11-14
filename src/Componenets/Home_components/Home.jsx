7bd086655f7721f041997b83c9a0bc302ddf97ea
Md-Sifat-code <codewithsifat4@gmail.com>
2024-11-14 15:20:21 +0600

    adding the react-fat-marquee

--- src/Componenets/Home_components/Home.jsx
+++ src/Componenets/Home_components/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 function Home() {
-  return <div>you are now at home</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="flex flex-row gap-3 items-center p-4 bg-gray-100">
+          <p className="btn btn-sm px-4 bg-[#D72050] font-bold text-white border-none">
+            Latest
+          </p>
+          <Marquee pauseOnHover>
+            <p className="mr-3">
+              <span className="font-bold">Match Highlights:</span> Germany vs
+              Spain — as it happened ! Match Highlights: Germany vs Spain as...
+            </p>
+            <p className="mr-3">
+              <span className="font-bold">Match Highlights:</span> Brazil vs
+              Bangladesh — as it happened ! Match Highlights: Brazil vs
+              Bangladesh as...
+            </p>
+            <p className="mr-3">
+              <span className="font-bold">Match Highlights:</span> England vs
+              Japan — as it happened ! Match Highlights: England vs Japan as...
+            </p>
+          </Marquee>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,18 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Componenets/Layout/Layout";
 import Error from "./Componenets/Fixed_componenet/Error";
+import Home from "./Componenets/Home_components/Home";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
     errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 createRoot(document.getElementById("root")).render(
